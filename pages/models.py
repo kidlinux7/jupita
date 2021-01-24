797d31261ec773596e584a221005dd46cfacacb8
centraspike <princeherman08@gmail.com>
2021-01-24 04:05:20 +0300

    Added content and slight changes🔥
    Content added on the homepage.
    Updates done from the Admin Panel as well.

--- pages/models.py
+++ pages/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 class Products(models.Model):
     title = models.CharField(max_length=200,null=True)
-    description = models.TextField(null=True, blank=True)
-    details = models.TextField(null=True, blank=True)
+    introduction = models.TextField(null=True, blank=True)
+    paragraph_1 = models.TextField(null=True, blank=True)
+    paragraph_2 = models.TextField(null=True, blank=True)
     playstore_link = models.CharField(max_length=200,null=True)
     appstore_link = models.CharField(max_length=200,null=True)
     website_link = models.CharField(max_length=200,null=True)
     product_pictures = models.ImageField(upload_to='products/')
     date_created = models.DateTimeField(auto_now=True)
-
 
     def __str__(self):
         return self.title
