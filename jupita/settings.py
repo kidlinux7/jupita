3a5c8020b7a2f90332c643af393b44912f278ea6
kidlinux7 <princeherman08@gmail.com>
2021-02-15 09:01:06 +0300

    Third Heroku Commit

--- jupita/settings.py
+++ jupita/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e^8jdmylzwck!d7=(e@lil$j@)6_6a6lrihvp=9%m_7qr8qy!)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0']
 
